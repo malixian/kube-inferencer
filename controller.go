7742e2656816be5337e14d4f821adf8ac012a936
malixian <1240609881@qq.com>
2020-04-09 16:37:43 +0800

    add controller

--- controller.go
+++ controller.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+
+	inferencerv1 "kube-inferencer/pkg/apis/inferencer/v1"
+	clientset "kube-inferencer/pkg/generate/clientset/versioned"
+	inferencerscheme "kube-inferencer/pkg/generate/clientset/versioned/scheme"
+	informers "kube-inferencer/pkg/generate/informers/externalversions/inferencer/v1"
+	listers "kube-inferencer/pkg/generate/listers/inferencer/v1"
+
+)
+
+// Controller is the controller implementation for inferencer resources
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	// networkclientset is a clientset for our own API group
+	networkclientset clientset.Interface
+
+	networksLister listers.NetworkLister
+	networksSynced cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
+
+// NewController returns a new network controller
+func NewController(
+	kubeclientset kubernetes.Interface,
+	networkclientset clientset.Interface,
+	networkInformer informers.NetworkInformer) *Controller {
+		
+}
--- main.go
+++ main.go
@@ -1,1 +1,56 @@
-package kube_inferencer
+package main
+
+import (
+	"flag"
+
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/kubernetes"
+	"github.com/golang/glog"
+	clientset "kube-inferencer/pkg/generate/clientset/versioned"
+	informers "kube-inferencer/pkg/generate/informers/externalversions"
+	"time"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+func main() {
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err :=  clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	inferencerClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+
+	inferencerInformerFactory := informers.NewSharedInformerFactory(inferencerClient, time.Second*30)
+
+	controller := NewController(kubeClient, inferencerClient,
+		inferencerInformerFactory.Inferencer().V1().Inferencers())
+
+	go inferencerInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		glog.Fatalf("Error running controller: %s", err.Error())
+	}
+
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
